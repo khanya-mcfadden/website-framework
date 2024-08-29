d55f06d24e268e77963c60c8c1c7eb8021ee2b3d
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-08-29 15:53:18 +0100

    moved files to diffrent folder

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact')
+def Contact():
+    return render_template('ContactPage.html')
+
+@app.route('/Booking')
+def Booking():
+    return render_template('BookingSeats.html')
+
+@app.route('/ordering')
+def ordering():
+    return render_template('Ordering.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
