5110366a38bb3581fc9380dad25f48571e11cc65
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-04 11:07:07 +0100

    added conmtent to login page

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def index():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
