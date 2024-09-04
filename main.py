ca7fd89f75bcf6882f26ff9d94d48571bc5662e7
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-04 10:43:56 +0100

    created new bratch

--- main.py
+++ main.py
@@ -6,19 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/contact')
-def Contact():
-    return render_template('ContactPage.html')
 
-@app.route('/Booking')
-def Booking():
-    return render_template('BookingSeats.html')
-
-@app.route('/ordering')
-def ordering():
-    return render_template('Ordering.html')
-@app.route('/testing')
-def testing(): 
-    return render_template('ordering.html')
 if __name__ == '__main__':
     app.run(debug=True)
