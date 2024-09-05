320fd7a2e406a9ebb27638c852ca9b1a8cc86cdb
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-05 17:11:51 +0100

    maded changes to main.py and others

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -10,123 +9,45 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-# Route to add a new stude user to the database
-@app.route("/register")
+# Add User route
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    # Get the input values from the form
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
 
-# Route to add a new record (INSERT) user data to the database
-@app.route("/addrec", methods = ['POST', 'GET'])
-def addrec():
-    # Data will be available from POST submitted by the form
-    if request.method == 'POST':
-        try:
-            username = request.form['usename']
-            email = request.form['email']
-            password = request.form['pass']
+    # Connect to the SQLite database
+    conn = sqlite3.connect("user.db")
+    cursor = conn.cursor()
 
-            # Connect to SQLite3 database and execute the INSERT
-            with sqlite3.connect('database.db') as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO user (username, email, pass) VALUES (?,?,?)",(username, email, password))
+    # Insert the user data into the database
+    add_data = data_add(username, email, password)
+    if add_data == True:
+        return redirect("/confirm")
+    else:
+        return redirect("/")
 
-                con.commit()
-                msg = "Record successfully added to database"
-        except:
-            con.rollback()
-            msg = "Error in the INSERT"
+    return redirect("/confirm")
+def data_add(username, email, password):
+    creation_date = datetime.datetime.now()
+    # Connect to the SQLite database
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    query = "insert into users (username, email, password, creation_date) values (?, ?, ?, ?)"
+    cursor.execute(query, (username, email, password, creation_date))
+    conn.commit()
+    conn.close()
 
-        finally:
-            con.close()
-            # Send the transaction message to result.html
-            return render_template('confirm.html',msg=msg)
+    return True
+@app.route("/confirm")
+def confirm():
+    return render_template("confirm.html")
 
-# Route to SELECT all data from the database and display in a table      
-@app.route('/list')
-def list():
-    # Connect to the SQLite3 datatabase and 
-    # SELECT rowid and all Rows from the students table.
-    con = sqlite3.connect("database.db")
-    con.row_factory = sqlite3.Row
+@app.route("/login", methods=["POST"])
+def login():
+    # Login logic here
+    pass
 
-    cur = con.cursor()
-    cur.execute("SELECT rowid, * FROM students")
-
-    rows = cur.fetchall()
-    con.close()
-    # Send the results of the SELECT to the list.html page
-    return render_template("list.html",rows=rows)
-
-# Route that will SELECT a specific row in the database then load an Edit form 
-@app.route("/edit", methods=['POST','GET'])
-def edit():
-    if request.method == 'POST':
-        try:
-            # Use the hidden input value of id from the form to get the rowid
-            id = request.form['id']
-            # Connect to the database and SELECT a specific rowid
-            con = sqlite3.connect("database.db")
-            con.row_factory = sqlite3.Row
-
-            cur = con.cursor()
-            cur.execute("SELECT rowid, * FROM students WHERE rowid = " + id)
-
-            rows = cur.fetchall()
-        except:
-            id=None
-        finally:
-            con.close()
-            # Send the specific record of data to edit.html
-            return render_template("edit.html",rows=rows)
-
-# Route used to execute the UPDATE statement on a specific record in the database
-@app.route("/editrec", methods=['POST','GET'])
-def editrec():
-    # Data will be available from POST submitted by the form
-    if request.method == 'POST':
-        try:
-            # Use the hidden input value of id from the form to get the rowid
-            rowid = request.form['rowid']
-            nm = request.form['nm']
-            addr = request.form['add']
-            city = request.form['city']
-            zip = request.form['zip']
-
-            # UPDATE a specific record in the database based on the rowid
-            with sqlite3.connect('database.db') as con:
-                cur = con.cursor()
-                cur.execute("UPDATE students SET name='"+nm+"', addr='"+addr+"', city='"+city+"', zip='"+zip+"' WHERE rowid="+rowid)
-
-                con.commit()
-                msg = "Record successfully edited in the database"
-        except:
-            con.rollback()
-            msg = "Error in the Edit: UPDATE students SET name="+nm+", addr="+addr+", city="+city+", zip="+zip+" WHERE rowid="+rowid
-
-        finally:
-            con.close()
-            # Send the transaction message to result.html
-            return render_template('result.html',msg=msg)
-
-# Route used to DELETE a specific record in the database    
-@app.route("/delete", methods=['POST','GET'])
-def delete():
-    if request.method == 'POST':
-        try:
-             # Use the hidden input value of id from the form to get the rowid
-            rowid = request.form['id']
-            # Connect to the database and DELETE a specific record based on rowid
-            with sqlite3.connect('database.db') as con:
-                    cur = con.cursor()
-                    cur.execute("DELETE FROM students WHERE rowid="+rowid)
-
-                    con.commit()
-                    msg = "Record successfully deleted from the database"
-        except:
-            con.rollback()
-            msg = "Error in the DELETE"
-
-        finally:
-            con.close()
-            # Send the transaction message to result.html
-            return render_template('result.html',msg=msg)
+if __name__ == "__main__":
+    app.run(debug=True)
