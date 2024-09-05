5325e83ad12814cafc930b836af8e9dfb7c62ff9
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-05 16:47:00 +0100

    chore: Update login page styling and form layout

--- main.py
+++ main.py
@@ -1,35 +1,132 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
 import sqlite3
-connection = sqlite3.connect("user.db")
-cursor = connection.cursor()
 
 app = Flask(__name__)
 
-
+# Home Page route
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
+# Route to add a new stude user to the database
 @app.route("/register")
 def register():
     return render_template("register.html")
 
+# Route to add a new record (INSERT) user data to the database
+@app.route("/addrec", methods = ['POST', 'GET'])
+def addrec():
+    # Data will be available from POST submitted by the form
+    if request.method == 'POST':
+        try:
+            username = request.form['usename']
+            email = request.form['email']
+            password = request.form['pass']
 
-@app.route("/ordering")
-def ordering():
-    return render_template("ordering.html")
+            # Connect to SQLite3 database and execute the INSERT
+            with sqlite3.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO user (username, email, pass) VALUES (?,?,?)",(username, email, password))
 
+                con.commit()
+                msg = "Record successfully added to database"
+        except:
+            con.rollback()
+            msg = "Error in the INSERT"
 
-@app.route("/booking")
-def booking():
-    return render_template("booking.html")
+        finally:
+            con.close()
+            # Send the transaction message to result.html
+            return render_template('confirm.html',msg=msg)
 
+# Route to SELECT all data from the database and display in a table      
+@app.route('/list')
+def list():
+    # Connect to the SQLite3 datatabase and 
+    # SELECT rowid and all Rows from the students table.
+    con = sqlite3.connect("database.db")
+    con.row_factory = sqlite3.Row
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    cur = con.cursor()
+    cur.execute("SELECT rowid, * FROM students")
+
+    rows = cur.fetchall()
+    con.close()
+    # Send the results of the SELECT to the list.html page
+    return render_template("list.html",rows=rows)
+
+# Route that will SELECT a specific row in the database then load an Edit form 
+@app.route("/edit", methods=['POST','GET'])
+def edit():
+    if request.method == 'POST':
+        try:
+            # Use the hidden input value of id from the form to get the rowid
+            id = request.form['id']
+            # Connect to the database and SELECT a specific rowid
+            con = sqlite3.connect("database.db")
+            con.row_factory = sqlite3.Row
+
+            cur = con.cursor()
+            cur.execute("SELECT rowid, * FROM students WHERE rowid = " + id)
+
+            rows = cur.fetchall()
+        except:
+            id=None
+        finally:
+            con.close()
+            # Send the specific record of data to edit.html
+            return render_template("edit.html",rows=rows)
+
+# Route used to execute the UPDATE statement on a specific record in the database
+@app.route("/editrec", methods=['POST','GET'])
+def editrec():
+    # Data will be available from POST submitted by the form
+    if request.method == 'POST':
+        try:
+            # Use the hidden input value of id from the form to get the rowid
+            rowid = request.form['rowid']
+            nm = request.form['nm']
+            addr = request.form['add']
+            city = request.form['city']
+            zip = request.form['zip']
+
+            # UPDATE a specific record in the database based on the rowid
+            with sqlite3.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute("UPDATE students SET name='"+nm+"', addr='"+addr+"', city='"+city+"', zip='"+zip+"' WHERE rowid="+rowid)
+
+                con.commit()
+                msg = "Record successfully edited in the database"
+        except:
+            con.rollback()
+            msg = "Error in the Edit: UPDATE students SET name="+nm+", addr="+addr+", city="+city+", zip="+zip+" WHERE rowid="+rowid
+
+        finally:
+            con.close()
+            # Send the transaction message to result.html
+            return render_template('result.html',msg=msg)
+
+# Route used to DELETE a specific record in the database    
+@app.route("/delete", methods=['POST','GET'])
+def delete():
+    if request.method == 'POST':
+        try:
+             # Use the hidden input value of id from the form to get the rowid
+            rowid = request.form['id']
+            # Connect to the database and DELETE a specific record based on rowid
+            with sqlite3.connect('database.db') as con:
+                    cur = con.cursor()
+                    cur.execute("DELETE FROM students WHERE rowid="+rowid)
+
+                    con.commit()
+                    msg = "Record successfully deleted from the database"
+        except:
+            con.rollback()
+            msg = "Error in the DELETE"
+
+        finally:
+            con.close()
+            # Send the transaction message to result.html
+            return render_template('result.html',msg=msg)
