6915dee1b35bdc70f3cb25bbbdb0ea5b3ad8eef3
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-05 14:40:07 +0100

    addded register page

--- main.py
+++ main.py
@@ -2,13 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/login')
+
+@app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
-if __name__ == '__main__':
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/ordering")
+def ordering():
+    return render_template("ordering.html")
+
+
+@app.route("/booking")
+def booking():
+    return render_template("booking.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
